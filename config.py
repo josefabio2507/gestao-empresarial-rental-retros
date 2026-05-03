167f1353ffe206a4ea6d4b265f292b4ec32104f6
josefabio2507 <josefabio2507@gmail.com>
2026-05-03 18:00:14 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "chave-dev-temporaria")
+    SECRET_KEY = os.getenv("SECRET_KEY", "chave-local-desenvolvimento")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///desenvolvimento.db"
-    )
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or "sqlite:///app.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
