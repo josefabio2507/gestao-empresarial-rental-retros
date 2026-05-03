c20175435e062113ed907e5c2047d58d1c089811
josefabio2507 <josefabio2507@gmail.com>
2026-05-03 18:14:33 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "chave-local-desenvolvimento")
 
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 
     if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
